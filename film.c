047a227ad5f279faba0dd55784b9e58ac1e19dc3
Veljd <121630812+Veljd@users.noreply.github.com>
2023-05-25 15:54:58 +0200

    Add files via upload

---
+++ film.c
@@ -0,0 +1,62 @@
+#include "film.h"
+#include <stdio.h>
+
+// Fonction pour sauvegarder un film dans un fichier
+void sauvegarderFilm(struct Film film, FILE *fichier) {
+  fprintf(fichier, "%s;%d;%s;%s\n", film.titre, film.annee, film.realisateur,
+          film.genre);
+}
+
+// Sauvegarde tous les films non vides dans un fichier CSV
+void sauvegarderFilms(struct Film *films) {
+  FILE *fichier = fopen("films.csv", "w");
+  if (fichier == NULL) {
+    printf("Erreur lors de l'ouverture du fichier.\n");
+    return;
+  }
+
+  int i = 0;
+  while (films[i].annee != -1) {
+    sauvegarderFilm(films[i], fichier);
+    i++;
+  }
+
+  fclose(fichier);
+}
+
+// Fonction pour afficher les films dans un tableau de films
+void afficherFilms(struct Film *films) {
+  int i = 0;
+  while (films[i].annee != -1) {
+    printf("Film %d :\n", i + 1);
+    printf("Titre : %s\n", films[i].titre);
+    printf("Année : %d\n", films[i].annee);
+    printf("Réalisateur : %s\n", films[i].realisateur);
+    printf("Genre : %s\n\n", films[i].genre);
+    i++;
+  }
+}
+
+void ajouterFilm(struct Film *films, int nbFilms) {
+  if (nbFilms >= MAX_FILMS) {
+    printf("Le tableau de films est plein.\n");
+    return;
+  }
+
+  struct Film nouveauFilm;
+
+  printf("----- Ajout d'un film -----\n");
+  printf("Titre : ");
+  scanf(" %[^\n]", nouveauFilm.titre);
+  printf("Année : ");
+  scanf("%d", &nouveauFilm.annee);
+  printf("Réalisateur : ");
+  scanf(" %[^\n]", nouveauFilm.realisateur);
+  printf("Genre : ");
+  scanf(" %[^\n]", nouveauFilm.genre);
+
+  films[nbFilms] = nouveauFilm;
+  (nbFilms)++;
+
+  printf("Le film a été ajouté avec succès.\n");
+}
